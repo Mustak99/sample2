8e7b26d104292af89c2cfe1cad0ca4ba6ed21a90
Mustak Shaikh <mustak.shaikh@techstile.in>
2023-07-28 18:31:33 +0530

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GanttEditorComponent, GanttEditorOptions } from 'ng-gantt';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<ng-gantt  [data]="data"></ng-gantt>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample2';
+  public editorOptions: GanttEditorOptions;
+  public data: any;
+  @ViewChild(GanttEditorComponent, { static: true }) editor!: GanttEditorComponent;
+
+  constructor() { 
+    this.editorOptions = new GanttEditorOptions()
+     this.data = [{
+      'pID': 1,
+      'pName': 'Define Chart API',
+      'pStart': '',
+      'pEnd': '',
+      'pClass': 'ggroupblack',
+      'pLink': '',
+      'pMile': 0,
+      'pRes': 'Brian',
+      'pComp': 0,
+      'pGroup': 1,
+      'pParent': 0,
+      'pOpen': 1,
+      'pDepend': '',
+      'pCaption': '',
+      'pNotes': 'Some Notes text'
+    }]; 
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgGanttEditorModule } from 'ng-gantt' 
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgGanttEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
